6682a7c49b753acb4641686051f5d14db9b2b7c8
evgeny.lesovskiy <evgeny.lesovskiy@uni.lu>
2025-12-03 15:58:25 +0100

    preprocessing and baseline

---
+++ src/2_preprocessing.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+
+def preprocessing():
+    df_raw = pd.read_csv('../data/raw/train.csv')
+
+    df_raw.drop("id", axis=1, inplace=True)
+    df_raw.drop(df_raw[df_raw["person_emp_length"] == 123].index, inplace=True)
+    df_raw.drop(df_raw[df_raw["person_age"] == 123].index, inplace=True)
+    df_raw.drop(df_raw[df_raw["person_emp_length"] > df_raw["person_age"] - 14].index, inplace=True)
+
+    df_raw = encode_with_one_hot_encoder(df_raw, "person_home_ownership")
+    df_raw = encode_with_one_hot_encoder(df_raw, "loan_intent")
+
+    df_raw = encode_with_ordinal_encoder(df_raw, "loan_grade", [['A', 'B', 'C', 'D', 'E', 'F', 'G']])
+    df_raw = encode_with_ordinal_encoder(df_raw, "cb_person_default_on_file", [['N', 'Y']])
+
+    df_raw.to_csv('../data/processed/train.csv', index=False)
+
+def encode_with_one_hot_encoder(df, column_name):
+    one_hot_encoder = OneHotEncoder(sparse_output=False)
+    encoded_column = one_hot_encoder.fit_transform(df[[column_name]])
+    new_columns = one_hot_encoder.get_feature_names_out([column_name])
+    df_encoded_home_ownership = pd.DataFrame(encoded_column, columns=new_columns, index=df.index)
+    return df.drop(columns=column_name).join(df_encoded_home_ownership)
+
+def encode_with_ordinal_encoder(df, column_name, categories):
+    ordinal_encoder = OrdinalEncoder(categories=categories)
+    df[column_name] = ordinal_encoder.fit_transform(df[[column_name]])
+    return df
+
+
+if __name__ == "__main__":
+    preprocessing()
---
+++ src/3_baseline.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import (train_test_split, StratifiedKFold, GridSearchCV)
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    roc_auc_score,
+    classification_report
+)
+import imblearn
+
+def baseline():
+    df = pd.read_csv("../data/processed/train.csv")
+    x = df.drop("loan_status", axis=1)
+    y = df["loan_status"]
+    x_train, x_val, y_train, y_val = train_test_split(
+        x, y, test_size=0.2, random_state=42, stratify=y
+    )
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    param_grid = {
+        'logisticregression__l1_ratio': np.linspace(0, 1, 10),
+        'logisticregression__C': np.logspace(-3, 2, 10)
+    }
+
+    pipeline_base = imblearn.pipeline.make_pipeline(StandardScaler(),
+                                                    LogisticRegression(random_state=42,
+                                                                       penalty='elasticnet',
+                                                                       class_weight='balanced',
+                                                                       solver='saga',
+                                                                       max_iter=5000)
+                                                    )
+
+    gs = GridSearchCV(pipeline_base,
+                      param_grid=param_grid,
+                      cv=skf,
+                      scoring='average_precision',
+                      n_jobs=-1)
+
+    gs.fit(x_train, y_train)
+    print(f'Лучшие параметры: {gs.best_params_}')
+    print(f'Лучшее качество на кросс-валидации: {gs.best_score_:.3f}')
+
+    y_pred_proba = gs.predict_proba(x_val)[:, 1]
+    roc_auc = roc_auc_score(y_val, y_pred_proba)
+    print(f"ROC-AUC: {roc_auc:.4f}")
+
+    y_pred = gs.predict(x_val)
+    report = classification_report(y_val, y_pred)
+    print("\nClassification Report:")
+    print(report)
+
+
+if __name__ == "__main__":
+    baseline()
